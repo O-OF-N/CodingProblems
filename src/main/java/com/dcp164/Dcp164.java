1a691d4db85a5226524c1e3495d801207771b7b4
Vinod <vinodm@spotify.com>
2018-12-27 12:55:57 -0500

    DCP 164

--- src/main/java/com/dcp164/Dcp164.java
+++ src/main/java/com/dcp164/Dcp164.java
@@ -1,17 +1,21 @@
 package com.dcp164;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+/**
+ * You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}.
+ * By the pigeonhole principle, there must be a duplicate. Find it in linear time and spac\
+ */
 
 public class Dcp164 {
 
 
   public int findDuplicate(Integer[] arr) {
-    for(int i=0;i<arr.length;i++) {
+    for (int i = 0; i < arr.length; i++) {
       int val = arr[Math.abs(arr[i])];
-      if(val>=0) {
-        arr[Math.abs(arr[i])] *=-1;
-      } else return Math.abs(arr[i]);
+      if (val >= 0) {
+        arr[Math.abs(arr[i])] *= -1;
+      } else {
+        return Math.abs(arr[i]);
+      }
     }
     return -1;
   }
